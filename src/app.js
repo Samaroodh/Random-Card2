5cc69d1dd880a029ee065cdad9c687ec47a09b02
Dulce <dulce.samaroo@gmail.com>
2022-10-25 23:33:09 +0000

    finished project

--- src/app.js
+++ src/app.js
@@ -9,3 +9,19 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+var suite = ["heart", "spade", "club", "diamond"];
+var cards = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+var numberElement = document.querySelector(".content");
+
+let cardIndex = Math.floor(Math.random() * cards.length);
+let suiteIndex = Math.floor(Math.random() * suite.length);
+
+console.log(cards[cardIndex]);
+if (suite[suiteIndex] === "spade" || suite[suiteIndex] === "club") {
+  numberElement.style.color = "black";
+} else {
+  numberElement.style.color = "red";
+}
+
+numberElement.innerHTML = cards[cardIndex];
+document.querySelector(".card").className = "card" + " " + suite[suiteIndex];
